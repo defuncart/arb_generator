a019099fee4f542adc67184ebfaec6185e03bf8a
James Leahy <defuncart@gmail.com>
2021-04-03 09:53:41 +0200

    POC using StringBuffer.

--- lib/src/services/arb_generator.dart
+++ lib/src/services/arb_generator.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:flappy_translator/src/services/file_writer/file_writer.dart';
 import 'package:flappy_translator/src/services/parsing/csv_parser.dart';
 import 'package:flappy_translator/src/services/validation/validator.dart';
 
@@ -39,6 +40,45 @@ abstract class ARBGenerator {
       );
     }
 
-    // TODO generate ARB
+    for (final supportedLanguage in supportedLanguages) {
+      final content = _generateARBFile(
+        langauge: supportedLanguage,
+        keys: parser.keys,
+        values: parser.getValues(supportedLanguage),
+        defaultValues: parser.defaultValues,
+      );
+
+      // write output file
+      final path = '${packageSettings.outputDirectory}/${packageSettings.filenamePrepend}$supportedLanguage.arb';
+      print(path);
+      FileWriter().write(contents: content, path: path);
+    }
   }
+}
+
+String _generateARBFile({
+  required String langauge,
+  required List<String> keys,
+  required List<String> values,
+  required List<String> defaultValues,
+}) {
+  if (keys.length != values.length && keys.length != defaultValues.length) {
+    print('Error! Mismatch number of keys and values');
+    exit(0);
+  }
+
+  final sb = StringBuffer();
+  sb.writeln('{');
+  sb.writeln('\t\"@@locale\": \"$langauge\",');
+  for (var i = 0; i < keys.length; i++) {
+    final value = i < values.length && values[i].isNotEmpty ? values[i] : defaultValues[i];
+    sb.write('\t\"${keys[i]}": \"$value\"');
+    if (i != keys.length - 1) {
+      sb.write(',');
+    }
+    sb.writeln();
+  }
+  sb.writeln('}');
+
+  return sb.toString();
 }
