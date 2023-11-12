9d1b9c3f23aeb188d6535c588404b7ceab67a76c
James Leahy <defuncart@gmail.com>
2023-11-12 11:02:45 +0100

    Feature/example use intl flutter gen over intl utils (#15)
    Example: use intl flutter-gen over intl_utils.

--- example/lib/widgets/my_app.dart
+++ example/lib/widgets/my_app.dart
@@ -1,6 +1,6 @@
-import 'package:example/generated/l10n.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -28,11 +28,11 @@ class _MyAppState extends State<MyApp> {
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
-      supportedLocales: AppLocalizations.delegate.supportedLocales,
+      supportedLocales: AppLocalizations.supportedLocales,
       locale: Locale(_locale),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('ARB Generator'),
+          title: const Text('arb_generator'),
         ),
         body: SingleChildScrollView(
           child: Column(
@@ -41,11 +41,9 @@ class _MyAppState extends State<MyApp> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  for (final locale
-                      in AppLocalizations.delegate.supportedLocales) ...[
+                  for (final locale in AppLocalizations.supportedLocales) ...[
                     ElevatedButton(
-                      onPressed: () =>
-                          setState(() => _locale = locale.toString()),
+                      onPressed: () => setState(() => _locale = locale.toString()),
                       child: Text(locale.toString()),
                     ),
                     const SizedBox(width: 8),
@@ -72,26 +70,26 @@ class HomeScreen extends StatelessWidget {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Text(AppLocalizations.of(context).myKey),
+        Text(AppLocalizations.of(context)!.myKey),
         const SizedBox(height: 8),
-        Text(AppLocalizations.of(context).welcome('Dash')),
+        Text(AppLocalizations.of(context)!.welcome('Dash')),
         const SizedBox(height: 8),
-        Text(AppLocalizations.of(context).numberMessages(0)),
-        Text(AppLocalizations.of(context).numberMessages(1)),
-        Text(AppLocalizations.of(context).numberMessages(2)),
-        Text(AppLocalizations.of(context).numberMessages(5)),
+        Text(AppLocalizations.of(context)!.numberMessages(0)),
+        Text(AppLocalizations.of(context)!.numberMessages(1)),
+        Text(AppLocalizations.of(context)!.numberMessages(2)),
+        Text(AppLocalizations.of(context)!.numberMessages(5)),
         const SizedBox(height: 8),
-        Text(AppLocalizations.of(context).whoseBook('male')),
-        Text(AppLocalizations.of(context).whoseBook('female')),
-        Text(AppLocalizations.of(context).whoseBook('other')),
+        Text(AppLocalizations.of(context)!.whoseBook('male')),
+        Text(AppLocalizations.of(context)!.whoseBook('female')),
+        Text(AppLocalizations.of(context)!.whoseBook('other')),
         const SizedBox(height: 8),
-        Text(AppLocalizations.of(context).unreadEmails(0, 'Dash')),
-        Text(AppLocalizations.of(context).unreadEmails(1, 'Dash')),
-        Text(AppLocalizations.of(context).unreadEmails(42, 'Dash')),
+        Text(AppLocalizations.of(context)!.unreadEmails(0, 'Dash')),
+        Text(AppLocalizations.of(context)!.unreadEmails(1, 'Dash')),
+        Text(AppLocalizations.of(context)!.unreadEmails(42, 'Dash')),
         const SizedBox(height: 8),
-        Text(AppLocalizations.of(context).weatherReaction('sunny')),
-        Text(AppLocalizations.of(context).weatherReaction('cloudy')),
-        Text(AppLocalizations.of(context).weatherReaction('rainy')),
+        Text(AppLocalizations.of(context)!.weatherReaction('sunny')),
+        Text(AppLocalizations.of(context)!.weatherReaction('cloudy')),
+        Text(AppLocalizations.of(context)!.weatherReaction('rainy')),
       ],
     );
   }
